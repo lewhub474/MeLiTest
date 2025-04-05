c2f695c292b54af3ff2a95c405fe6e15c828ae1f
lewhub474 <lewmxt615@gmail.com>
2025-04-04 23:36:20 -0500

    Initial commit

---
+++ MeLiTestPedroF/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeLiTestPedroF
+//
+//  Created by Macky on 4/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeLiTestPedroF/MeLiTestPedroFApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeLiTestPedroFApp.swift
+//  MeLiTestPedroF
+//
+//  Created by Macky on 4/04/25.
+//
+
+import SwiftUI
+
+@main
+struct MeLiTestPedroFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
